0ad34b6d17ba55cd98d5ca0fcd9aa851960408ae
irma <ii@pryaniky.ru>
2022-03-01 11:52:08 +0300

    muiMaskPhone

--- src/Authorisation/Authorisation.js
+++ src/Authorisation/Authorisation.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import './Authorisation.css'
-
+import MuiPhoneNumber from 'material-ui-phone-number';
 const AuthorizationForm = () => {
     // state функционального компонента пароль, логин и.... 
     const [password, setPassword] = useState('')
     const [login, setLogin] = useState('')
     // состояние скрытия компонента авторизации
     const [authIsValid, setAuthIsValid] = useState(false)
-
+const [state, setState] =useState({phone:""})
     // функция проверяющая соответсвие пароля установленным и меняющая state если пароль и логин введен правильно
     const AuthorisationIsValid = (login, password) => {
         if (login === "irma@gmail.com" && password === "12345678") {
@@ -18,8 +18,10 @@ const AuthorizationForm = () => {
             alert("bad decision")
         }
     }
-
-
+    function handleOnChange(value) {
+      setState({phone:value});
+     }
+console.log(state)
     return (
         <div style={{ display: authIsValid ? "none" : "flex" }} className="AuthForm">
             <div className="dws-form">
@@ -34,6 +36,9 @@ const AuthorizationForm = () => {
                     <div className="box-input">
                         <input className="input" aria-label="Введите пароль" type="password" required onChange={(e) => setPassword(e.currentTarget.value)} placeholder={password} />
                         <label >Введите пароль</label>
+                    </div>
+                    <div>
+                    <MuiPhoneNumber  variant="outlined" label="Phone" className="InputWithMask" inputClass autoFormat defaultCountry={'us'} onChange={handleOnChange}/>
                     </div>
                     {/* кнопка загрузки */}
                     <a href="#" className="button"  onClick={() => AuthorisationIsValid(login, password)}>Войти</a>
